9d171e74e70777596873aa55318c318a7eb212f7
рома <138679614+romtyx@users.noreply.github.com>
2023-07-09 13:42:34 +0300

    Update pingpong.py
    create perent class for sprites

--- pingpong.py
+++ pingpong.py
@@ -1,1 +1,10 @@
+from pygame import *
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, hight):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, hight))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
